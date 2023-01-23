8e3a13bded3f640a58a5ffd78fc5064770e519cf
Joshua Crawford <joshua.crawford.mail@gmail.com>
2023-01-23 16:15:23 -0500

    Adding migration for User

--- leads/models.py
+++ leads/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-from django.contrib.auth.models from AbstractUser
+from django.contrib.auth.models import AbstractUser
 
-class User()
+
+class User(AbstractUser):
+    pass
+
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
@@ -10,9 +13,5 @@ class Lead(models.Model):
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
 
-
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    
