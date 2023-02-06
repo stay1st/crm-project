88fbc34a357a22024463c883658b7f995d61123e
Joshua Crawford <joshua.crawford.mail@gmail.com>
2023-02-05 20:45:41 -0500

    Fixed the E304 error on 'User' model

--- examplecrm/settings.py
+++ examplecrm/settings.py
@@ -82,6 +82,10 @@ DATABASES = {
     }
 }
 
+# User authentication
+
+AUTH_USER_MODEL = 'leads.User'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
