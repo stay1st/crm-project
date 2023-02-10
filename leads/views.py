5fd3c7f262c70ec915e1f7c15c5b82c6abf0e77e
Joshua Crawford <joshua.crawford.mail@gmail.com>
2023-02-09 22:27:10 -0500

    Adding Views and paths to database $ Edited VSCode workspace settings for html editor to be recognized $ Renamed home_page to lead_list and added styles $ Initiated the django for loop to display lead list

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 
-def home_page(request):
-    # return HttpResponse("Hello World!")
-    return render(request, "second_page.html")
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    print(pk)
+    return HttpResponse("Here is the detail view")
