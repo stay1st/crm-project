c48c7e0cfc8d687d604b5d3e2c48fba6ea7b8baa
Joshua Crawford <joshua.crawford.mail@gmail.com>
2023-02-26 20:05:08 -0500

    Creating lead and lead page to post to the database

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
 from .forms import LeadForm
 
 
@@ -21,7 +21,24 @@ def lead_detail(request, pk):
 
 
 def lead_create(request):
+    if request.method == "POST":
+        print('Receiveing a post request')
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print("The form is valid")
+            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent
+            )
+            print("Lead has been created")
     context = {
-        "form": LeadForm()
+        "form": form
     }
     return render(request, "leads/lead_create.html", context)
